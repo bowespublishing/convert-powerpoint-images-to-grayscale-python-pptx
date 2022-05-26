728a62069956566c1cd42fb4e514d6951c14cf89
bowespublishing <80654666+bowespublishing@users.noreply.github.com>
2022-05-26 01:36:49 +0100

    Add files via upload

---
+++ grayscale.py
@@ -0,0 +1,30 @@
+from pptx import Presentation
+from pptx.oxml.xmlchemy import OxmlElement
+
+
+def AddGrayscale(shape):
+
+    pic = shape._pic
+    rId = pic.xpath('./p:blipFill/a:blip')[0]
+    jj = OxmlElement("a:grayscl")
+    rId.insert(0,jj)
+
+
+if __name__ == "__main__":
+
+
+    file = "grayscale.pptx"
+
+
+    presentation = Presentation(file)
+
+
+    startslide = 1
+    endslide = len(presentation.slides)
+
+    for sl in range(startslide, endslide + 1):
+        slide = presentation.slides[sl - 1]
+        for shape in slide.shapes:
+            AddGrayscale(shape)
+
+    presentation.save(file)
